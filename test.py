bc9e0ea411569f63fd7e8efff8b9e86016ffa69d
Caio <ccampos10x@gmail.com>
2026-02-02 00:46:42 -0300

    name and houses test

--- test.py
+++ test.py
@@ -1,21 +1,11 @@
-def main():
-	number = get_number()
-	meow(number)
+students = [
+	{"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+	{"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+	{"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russel terrier"},
+	{"name": "Draco", "house": "Slytherin", "patronus": None} # None when is empty
+]
 
-def get_number():
-	while True:
-		n = int(input("What's n? "))
-		if n > 0:
-			break
-	return n
-
-def meow(n):
-	for _ in range (n):
-		print("meow")
-		print("meow2")
-		print("meow3")
-		print("meow4")
-		print("meow5")
+for student in students:
+	print(student["name"], student["house"], student["patronus"], sep=", ") # can print each data you want
 
 
-main()
